51b308717bbfa9936a43f8e097c54f98103a0127
MithunBurman7 <mithunburman7@gmail.com>
2017-06-22 14:40:13 +0530

    Session5-Assignment5.1

---
+++ src/Arreys/Arreys.java
@@ -0,0 +1,23 @@
+package Arreys;
+
+public class Arreys {
+
+	public static void main(String[] args) {
+
+				int[] arr = new int [10];// took int is 10
+				
+		        
+		 
+		        System.out.println("Printing Even numbers between 1 to " + 10);
+		       
+		        for(int i=1; i <= 10; i++){
+		               
+		                // if the number is divisible by 2 then it is even
+		                
+		        	if( i % 2 == 0){
+		                        System.out.print(i + " ");
+		                }
+		        }
+			}
+		}
+
